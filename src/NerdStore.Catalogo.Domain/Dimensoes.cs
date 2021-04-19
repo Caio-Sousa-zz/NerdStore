c41b09219ee6a5e8638a743242b49c75d8b2fe82
Caio sousa <caiyo.projeto@gmail.com>
2021-04-19 14:57:46 -0300

    Unit test catalogo domain.

--- src/NerdStore.Catalogo.Domain/Dimensoes.cs
+++ src/NerdStore.Catalogo.Domain/Dimensoes.cs
@@ -13,11 +13,11 @@ namespace NerdStore.Catalogo.Domain
 
         public Dimensoes(decimal altura, decimal largura, decimal profundidade)
         {
-            Validar();
-
             Altura = altura;
             Largura = largura;
             Profundidade = profundidade;
+
+            Validar();
         }
 
         public string DescricaoFormatada() => $"LxAxP: {Largura} x {Altura} x {Profundidade}";
@@ -30,8 +30,8 @@ namespace NerdStore.Catalogo.Domain
         private void Validar()
         {
             Validacoes.ValidarSeMenorQue(Altura, 1, "O campo Altura não pode ser menor ou igual a 0");
-            Validacoes.ValidarSeMenorQue(Altura, 1, "O campo Largura não pode ser menor ou igual a 0");
-            Validacoes.ValidarSeMenorQue(Altura, 1, "O campo Profundidade não pode ser menor ou igual a 0");
+            Validacoes.ValidarSeMenorQue(Largura, 1, "O campo Largura não pode ser menor ou igual a 0");
+            Validacoes.ValidarSeMenorQue(Profundidade, 1, "O campo Profundidade não pode ser menor ou igual a 0");
         }
     }
 }
