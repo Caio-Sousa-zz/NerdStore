d3ba0ffd908fd203971d091299875771231c189e
Caio sousa <caiyo.projeto@gmail.com>
2021-05-08 10:50:25 -0300

    Data Access Implementation.

--- docs/src/NerdStore.Catalogo.Domain/Categoria.cs
+++ docs/src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -1,18 +1,17 @@
-﻿using NerdStore.Core.DomainObjects;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using NerdStore.Core.DomainObjects;
 
 namespace NerdStore.Catalogo.Domain
 {
     public class Categoria : Entity
     {
         public string Nome { get; private set; }
-
         public int Codigo { get; private set; }
-
-        public Categoria() { }
 
         // EF Relation
         public ICollection<Produto> Produtos { get; set; }
+
+        protected Categoria() { }
 
         public Categoria(string nome, int codigo)
         {
@@ -29,7 +28,7 @@ namespace NerdStore.Catalogo.Domain
 
         public void Validar()
         {
-            Validacoes.ValidarSeVazio(Nome, "O campo Nome do produto não pode estar vazio");
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome da categoria não pode estar vazio");
             Validacoes.ValidarSeIgual(Codigo, 0, "O campo Codigo não pode ser 0");
         }
     }
