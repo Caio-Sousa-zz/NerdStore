3fe18f3255e2efa3a2e525138dd0826e48d7b0f1
Caio sousa <caiyo.projeto@gmail.com>
2021-05-08 14:25:45 -0300

    Application Layer.

---
+++ src/NerdStore.Catalogo.Application/Services/IProdutoAppService.cs
@@ -0,0 +1,21 @@
+﻿using NerdStore.Catalogo.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Application.Interface
+{
+    public interface IProdutoAppService : IDisposable
+    {
+        Task<IEnumerable<ProdutoDto>> ObterPorCategoria(int codigo);
+        Task<ProdutoDto> ObterPorId(Guid id);
+        Task<IEnumerable<ProdutoDto>> ObterTodos();
+        Task<IEnumerable<CategoriaDto>> ObterCategorias();
+
+        Task AdicionarProduto(ProdutoDto ProdutoDto);
+        Task AtualizarProduto(ProdutoDto ProdutoDto);
+
+        Task<ProdutoDto> DebitarEstoque(Guid id, int quantidade);
+        Task<ProdutoDto> ReporEstoque(Guid id, int quantidade);
+    }
+}
