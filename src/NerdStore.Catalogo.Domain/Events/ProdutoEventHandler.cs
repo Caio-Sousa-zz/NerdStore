d8d18882be6828a2c5cc90f70aeb2b471b0711e6
Caio sousa <caiyo.projeto@gmail.com>
2021-05-08 12:05:16 -0300

    Event Service Layer.

---
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using NerdStore.Catalogo.Domain.Interface;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    public class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoEventHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Handle(ProdutoAbaixoEstoqueEvent messagem, CancellationToken cancellationToken)
+        {
+            var produto = await _produtoRepository.ObterPorId(messagem.AggregateId);
+
+            // Enviar um email para xxx
+                                 
+        }
+    }
+}
