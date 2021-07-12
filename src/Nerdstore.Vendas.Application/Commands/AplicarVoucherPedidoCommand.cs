dabf78d28b5e46aa257c15d40a3f380a3cb4a9a4
Caio sousa <caiyo.projeto@gmail.com>
2021-07-12 07:54:31 -0300

    Vendas Commands

---
+++ src/Nerdstore.Vendas.Application/Commands/AplicarVoucherPedidoCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentValidation;
+using NerdStore.Core.Messages;
+
+namespace Nerdstore.Vendas.Application.Commands
+{
+    public class AplicarVoucherPedidoCommand : Command
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public string CodigoVoucher { get; private set; }
+
+        public AplicarVoucherPedidoCommand(Guid pedidoId, Guid clienteId, string codigoVoucher)
+        {
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+            CodigoVoucher = codigoVoucher;  
+        }
+
+        public override bool EhValido()
+        {
+            ValidationResult = new AplicarVoucherPedidoValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+
+    public class AplicarVoucherPedidoValidation : AbstractValidator<AplicarVoucherPedidoCommand>
+    {
+        public AplicarVoucherPedidoValidation()
+        {
+            RuleFor(c => c.ClienteId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do cliente inválido");
+
+            RuleFor(c => c.PedidoId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do pedido inválido");
+
+            RuleFor(c => c.CodigoVoucher)
+                .NotEmpty()
+                .WithMessage("O código do voucher não pode ser vazio");
+        }
+    }
+}
---
+++ src/Nerdstore.Vendas.Application/Commands/RemoverItemPedidoCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentValidation;
+using NerdStore.Core.Messages;
+
+namespace Nerdstore.Vendas.Application.Commands
+{
+    public class RemoverItemPedidoCommand : Command
+    {
+        public Guid ClienteId { get; }
+        public Guid PedidoId { get; }
+        public Guid ProdutoId { get; }
+
+        public RemoverItemPedidoCommand(Guid clienteId, Guid pedidoId, Guid produtoId)
+        {
+            ClienteId = clienteId;
+            PedidoId = pedidoId;
+            ProdutoId = produtoId;
+        }
+
+        public override bool EhValido()
+        {
+            ValidationResult = new RemoverItemPedidoValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+
+    public class RemoverItemPedidoValidation : AbstractValidator<RemoverItemPedidoCommand>
+    {
+        public RemoverItemPedidoValidation()
+        {
+            RuleFor(c => c.ClienteId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do cliente inválido");
+
+            RuleFor(c => c.ProdutoId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do produto inválido");
+
+            RuleFor(c => c.PedidoId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do pedido inválido");
+        }
+    }
+}
